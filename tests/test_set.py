84f25c5a1a090268eda341ef871a28823313a515
Lars Buitinck <larsmans@gmail.com>
2011-12-16 14:58:09 +0100

    ENH test for union, seems not to segfault

--- tests/test_set.py
+++ tests/test_set.py
@@ -2,9 +2,20 @@ from nose.tools import assert_equal
 
 from sortedcollections import SortedSet
 
+
 def test_sortedset():
     a = SortedSet([2, 3])
     a.add(1)
     a.discard(2)
 
     assert_equal(list(a), [1, 3])
+
+
+def test_union():
+    a = SortedSet([1, 2])
+    b = set([2, 3])
+
+    assert_equal(list(a | b), [1, 2, 3])
+
+    a |= b
+    assert_equal(list(a), [1, 2, 3])
